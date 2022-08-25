b48e84223193a203553fd8b0d9dad66a16250790
Muhammad Yusuf <92169759+muhdyusuf@users.noreply.github.com>
2022-08-26 01:42:15 +0800

    Add files via upload

---
+++ Jobsheet Generator Web/jobsheet.js
@@ -0,0 +1,41 @@
+
+(()=>{
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+
+  const {nolog}=params
+
+  try {
+    Object.keys(params).map(param=>{
+      if(params[param]){
+          document.getElementById(param).innerText=params[param]
+        }      
+      
+      else{
+        return
+      }
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+
+  const element=document.querySelector("#element")
+  var opt = {
+    margin:       0.3,
+    filename:  nolog? `${nolog}.pdf` : 'jobsheet.pdf',
+    image:        { type: 'jpeg', quality: 1 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { unit: 'in', format: 'A4', orientation: 'portrait' }
+  };
+   
+  
+  html2pdf().from(element).set(opt).save()
+  console.log(params)
+  
+})()
+
+
+
+
+
+
