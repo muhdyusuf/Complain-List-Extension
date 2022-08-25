f25572547e133d7e086c5b5cb17751fb89496876
muhdyusuf <92169759+muhdyusuf@users.noreply.github.com>
2022-08-26 07:16:08 +0800

    Update jobsheet.js

--- Jobsheet Generator Web/jobsheet.js
+++ Jobsheet Generator Web/jobsheet.js
@@ -3,7 +3,7 @@
   const urlSearchParams = new URLSearchParams(window.location.search);
   const params = Object.fromEntries(urlSearchParams.entries());
 
-  const {nolog}=params
+  const {noLog}=params
 
   try {
     Object.keys(params).map(param=>{
@@ -22,7 +22,7 @@
   const element=document.querySelector("#element")
   var opt = {
     margin:       0.3,
-    filename:  nolog? `${nolog}.pdf` : 'jobsheet.pdf',
+    filename:  noLog? `${noLog}.pdf` : 'jobsheet.pdf',
     image:        { type: 'jpeg', quality: 1 },
     html2canvas:  { scale: 4 },
     jsPDF:        { unit: 'in', format: 'A4', orientation: 'portrait' }
