022da44d8f66a04abb4930dd2e8df4300474f94a
Muhammad Yusuf <92169759+muhdyusuf@users.noreply.github.com>
2023-04-26 23:52:28 +0800

    fix personal number bug and add time on masa lapor

--- dist/content-script.js
+++ dist/content-script.js
@@ -9,10 +9,10 @@
         if(row.length>=10){
             for(let i=1;i<=row.length;i++){
                 const pdfbutton=document.createElement("button")
-                pdfbutton.classList.add("pdf-btn")
+                pdfbutton.classList.add("pdf-btn","mt-4")
                 pdfbutton.innerText="generate pdf"
                 const secondCol=document.querySelector(`tbody>tr:nth-child(${i})>td:nth-child(2)`)
-                pdfbutton.onclick=()=>generatePdf(i)
+                pdfbutton.onclick=()=>generatePdf(i<10?i:i<20?i-10:i-20)
                 secondCol.appendChild(pdfbutton)
             }
             observer.disconnect()
@@ -24,7 +24,6 @@
 
     const config = { attributes: true, childList: true, subtree: true };
 
-   console.log("hi")
     observer.observe(target,config)
 
 
@@ -44,74 +43,97 @@
 
     
     const col3=(()=>{
-        const data=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(3)`).innerText.split("\n")
-        let pejabat=data[2].replace("P : ","")
-        let bimbit=data[3].replace("B : ","")
-        const noTel=bimbit!==pejabat?`${pejabat} / ${bimbit}` : bimbit
+        let data
+        try {
+            data=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(3)`).innerText.split("\n")
+            let pejabat=data[2].replace("P : ","")
+            pejabat=pejabat=="P : "|| ""
+            let bimbit=data[3].replace("B : ","")
+            const noTel=bimbit!==pejabat && pejabat?`${pejabat} / ${bimbit}` : bimbit
+         
+            data.push(noTel)
+        } catch (error) {
+            data=['','']
+        }
     
-        return {nama:data[0],noTel}
+        return {nama:data[0],noTel:data[data.length-1]}
     })()
     
     
     const col4= (()=>{
-        const data=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(4)`).innerText.split("\n")
-        const tagId=(()=>{
+        let data=['','','']
+        let tagId
+        try {
+            data=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(4)`).innerText.split("\n")
+            tagId=(()=>{
             if(!data)return("")
-            if(!data[0].match(/sdk\/0[0-6]\/[0-9]{3}/ig))return ("")
-            return data[0].match(/sdk\/0[0-6]\/[0-9]{3}/ig)
-        })()
+            if(!data[0].match(/(sdk|kk|kdt|kng)\/0[0-6]\/[0-9]{3,4}/ig))return ("")
+            return data[0].match(/(sdk|kk|kdt|kng)\/0[0-6]\/[0-9]{3,4}/ig)
+           })()
+        } catch (error) {
+            data=['','','']
+            tagId=''
+            
+        }
+
         return {tagId,catatan:data[1],masalah:data[2],perincianMasalah:data[2]}
     
     })()
     
     const col6=(()=>{
         function getTime(text){
+            console.log((text,row))
             let [date,time]=text.split("-")
+            let responseTime=date.split(",")[2]
             date=date.split(",").splice(0,2).join("").replace(/(P|O|I)rt\s:\s/ig,"")? (date.split(",").splice(0,2).join("").replace(/(P|O|I)rt\s:\s/ig,"")):""
-
+            
             time=time.match(/\d\d:\d\d(a|p)m/ig)[0]?time.match(/\d\d:\d\d(a|p)m/ig)[0]:""
-            return [date,time]
+            return {date,time,responseTime}
         }
 
-        const irtText=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6)>li`).innerText
-        const ortText=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6)>li:nth-of-type(2)`).innerText
-        const prtText=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6)>li:nth-of-type(3)`).innerText
-
+  
+       
 
      
-
-        
-
         // const irt=date+" @ "+time
         // const masaLapor=date + " @"
 
         let [irt,masaLapor,ort,prt,tindakanpembaikan]=["","","","",""]
 
         try {
-            irt=getTime(irtText).join(" @ ")
-            masaLapor=getTime(irtText).slice(0,1).join("")+" @ "
+
+            const irtText=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6) li`) && document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6) li`).innerText
+
+       
+            const {date,time,responseTime}=getTime(irtText)
+
+            irt=`${date} @ ${time}`
+            masaLapor=`${date} @ ${responseTime}`
+
+          
 
             if(window.location.href==="http://www.syarikatjnl.com/jknshelpdesk/complaint_list.php?archived=1"){
+            const ortText=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6)>li:nth-of-type(2)`).innerText
+            const prtText=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6)>li:nth-of-type(3)`).innerText
             ort=getTime(ortText).join(" @ ")
             prt=getTime(prtText).join(" @ ")
             tindakanpembaikan=document.querySelector(`tbody>tr:nth-child(${row})>td:nth-child(6)>li:nth-last-child(1)`).innerText.split("\n")
             tindakanpembaikan=tindakanpembaikan.slice(3,tindakanpembaikan.length-1).join(" ")
 
-        }
+            }
 
         } catch (error) {
             console.log(error)
             
         }
         
-        return {irt,masaLapor,ort,prt,tindakanpembaikan}
+        return {irt:irt?irt:"",masaLapor,ort,prt,tindakanpembaikan}
     })()
 
     
     const data= Object.assign(col2,col3,col4,col6)
-
-    const searchParams=new URLSearchParams(data).toString()
-
+    console.log(new URLSearchParams(data))
+    const searchParams=new URLSearchParams(data)
     chrome.runtime.sendMessage({redirect:`./index.html?${searchParams}`});
 
     
